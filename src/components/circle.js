1e38d50d6f2dc269d76de19149b1619fefddf940
Animesh Srivastava <85071104+Aman-Animesh@users.noreply.github.com>
2022-02-28 22:10:18 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
-// import using require
-
+var shape = require('./shape');
 // declare class
 
+class Circle extends shape{
+    calculateArea() {
+        return 3.14*4*4;
 
-// export class using module.exports
+    }  
+}
+
+module.exports = Circle
